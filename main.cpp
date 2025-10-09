ab3b514c8bc86b8dfe7bbb746075dd64b310ab47
Poul-png <Zavitaevpavel7@gmail.com>
2025-10-09 13:48:09 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+int main(){
+	int in1 = 1;
+	int in2 = 2;
+	int in3 = 3;
+	int in4 = 4;
+	int in5 = 5;
+	std::cout << in1 << in2 << in3 << in4 << in5 << std::endl;
+
+	short sh1 = 1;
+	short sh2 = 2;
+	short sh3 = 3;
+	short sh4 = 4;
+	short sh5 = 5;
+	std::cout << sh1 << sh2 << sh3 << sh4 << sh5 << std::endl;
+
+	double db1 = 1;
+	double db2 = 2;
+	double db3 = 3;
+	double db4 = 4;
+	double db5 = 5;
+	std::cout << db1 << db2 << db3 << db4 << db5 << std::endl;
+
+	float fl1 = 1;
+	float fl2 = 2;
+	float fl3 = 3;
+	float fl4 = 4;
+	float fl5 = 5;
+	std::cout << fl1 << fl2 << fl3 << fl4 << fl5 << std::endl;
+
+	bool b1 = 1;
+	bool b2 = 0;
+	bool b3 = 1;
+	bool b4 = 0;
+	bool b5 = 1;
+	std::cout << b1 << b2 << b3 << b4 << b5 << std::endl;
+
+	char ch1 = 'a';
+	char ch2 = 'b';
+	char ch3 = 'c';
+	char ch4 = 'd';
+	char ch5 = 'e';
+	std::cout << ch1 << ch2 << ch3 << ch4 << ch5 << std::endl;
+
+	std::string st1 = "a";
+	std::string st2 = "b";
+	std::string st3 = "c";
+	std::string st4 = "d";
+	std::string st5 = "e";
+	std::cout << st1 << st2 << st3 << st4 << st5 << std::endl;
+}
