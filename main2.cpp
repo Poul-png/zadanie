4fe01877a520a3299d4f91a9398a5232a4bef5c4
Poul-png <Zavitaevpavel7@gmail.com>
2025-10-09 14:58:07 +0300

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -7,28 +7,23 @@ int main() {
 	std::string previous = " ";
 	std::string current;
 
-	
-	int a;
-	std::cin >> a;
-	if (a == 1) {
-		std::cout << "Сейчас лето одевайтесь легко" << std::endl;
+	while (true) {
+		int a;
+		std::cin >> a;
+		if (a == 1) {
+			std::cout << "Сейчас лето одевайтесь легко" << std::endl;
+		}
+		else if (a == 2) {
+			std::cout << "Сейчас осень одевайтесь теплее и возьмите зонт" << std::endl;
+		}
+		else if (a == 3) {
+			std::cout << "Сейчас зима одевайтесь очень тепло" << std::endl;
+		}
+		else if (a == 4) {
+			std::cout << "Сейчас весна одевайтесь тепло и возьмите зонт" << std::endl;
+		}
+		else {
+			std::cout << "Неверно" << std::endl;
+		}
 	}
-	else if(a == 2) {
-		std::cout << "Сейчас осень одевайтесь теплее и возьмите зонт"<<std::endl;
-	}
-	else if (a == 3) {
-		std::cout << "Сейчас зима одевайтесь очень тепло" << std::endl;
-	}
-	else if (a == 4) {
-		std::cout << "Сейчас весна одевайтесь тепло и возьмите зонт" << std::endl;
-	}
-	else {
-		std::cout << "Неверно" << std::endl;
-	}
-
-
-
-
-
-
 }
