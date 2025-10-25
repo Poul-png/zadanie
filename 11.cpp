42592ae28667f97984732d731dd88647e47e801a
Poul-png <Zavitaevpavel7@gmail.com>
2025-10-25 23:00:31 +0300

    Create 11.cpp

---
+++ 11.cpp
@@ -0,0 +1,36 @@
+#include <iostream>  
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "Rus");
+
+	int a;
+	int length;
+	int userFigure;
+
+	cout << "[ === ПРОГРАММА - 'Геометрические фигуры' === ]" << endl   << "[ 1 ] Линия"<< endl << "[ > ] Выберите фигуру: ";
+	cin >> userFigure;
+	if (userFigure == 1) {
+		cout << "[ > ] Введите длину фигуры: ";
+		cin >> length;
+		if (length > 0) {
+			
+			while (length != 0) {
+				cout << "#";
+				length = length - 1;
+			}
+		}
+		else {
+
+			cout << "[ - ] Введена некорректная длина!";
+
+		}
+	}
+	else {
+		cout << "[ + ] Такой фигуры не существует!";
+	}
+}
