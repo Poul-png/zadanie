aa392ff6ab2f1e404a3c2b7109b6ee803198c841
Poul-png <Zavitaevpavel7@gmail.com>
2025-11-28 14:39:29 +0300

    Create taable.cpp

---
+++ taable.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+
+	int usNum1, usNum2, usNum3, usNum4, usNum5;
+
+	cout << "== Одномерные масиивы ==\n" << "[ > ] Введите 5 чисел:" << endl;
+
+	cin >> usNum1 >> usNum2 >> usNum3 >> usNum4 >> usNum5;
+
+	int usIn[5] = { usNum1, usNum2, usNum3, usNum4, usNum5 };
+
+
+}
